3481164595c095bd1232b646888a4028ae1fe1ef
kkkkan <kan4649kan@gmail.com>
2021-11-14 13:11:08 +0900

    feat : 赤字にあっているのがQRの内容であることを説明する文章を追加

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,19 @@
 <template>
   <div class="overflow-x-auto">
     <!--overflow-x-autoを指定しないと縦スクロールバーがQrItemのコンテンツにかぶってしまった。-->
+
     <label>
       <input type="file" @change="onUpload" />
     </label>
 
     <!--エラーメッセージがあったら表示-->
     <p v-if="this.errorMessage" class="mt-4">{{ errorMessage }}</p>
+
+    <!--エラーメッセージがなく、csvがアップロードされていたら表示-->
+    <p v-if="!this.errorMessage && this.workers.length > 0" class="mt-4">
+      <span class="red--text font-weight-black"> 赤字 </span>
+      <span class="font-weight-black"> になっているのが、QRの内容です。 </span>
+    </p>
 
     <ul>
       <div v-for="(item, index) in workers" :key="item" class="item">
