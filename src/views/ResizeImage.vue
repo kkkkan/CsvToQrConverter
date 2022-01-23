567f81e312d71aea1893c83b72a96cb798527409
kkkkan <kan4649kan@gmail.com>
2022-01-23 18:41:29 +0900

    refactor : コード整理

--- src/views/ResizeImage.vue
+++ src/views/ResizeImage.vue
@@ -6,9 +6,8 @@
       アップロードした画像をリサイズします。画像拡張子はpngのみ対応しています。
     </div>
 
-
     <p v-if="this.error_message" class="mt-4 font-weight-bold indigo--text">
-      変換後サイズ : 
+      変換後サイズ :
     </p>
 
     <label>
@@ -31,7 +30,7 @@
     </p>
 -->
     <!--文字コードの設定-->
-     <!--<p class="mt-4 mb-4">
+    <!--<p class="mt-4 mb-4">
       <span>文字コード:</span>
 
       <span>
@@ -71,7 +70,7 @@
     </p>
 -->
     <!--CSVファイルの全列数より大きな数字を指定した場合の挙動の説明。文字は小さくして、少し奥から始める。-->
-   <!--  <p class="text-body2 ml-3">
+    <!--  <p class="text-body2 ml-3">
       ※アップロードしたCSVの列数より大きい数を指定した場合、最後の列がQRコード画像になります。
     </p>
 -->
@@ -90,7 +89,7 @@
     </p>
 -->
     <!--QR一覧-->
-  <!--  <ul>
+    <!--  <ul>
       <div v-for="(item, index) in workers" :key="item" class="item">
         <qr-item
           class="mt-15 mb-15"
@@ -105,21 +104,19 @@
 </template>
 
 <script>
-
 export default {
   name: 'ResizeImage',
 
   data() {
     return {
       error_message: '', // エラーメッセージ
-      file: null, // アップロードされたFile　
-      canvas : null,
-      context:null,
+      file: null, // アップロードされたFile
+      canvas: null,
+      context: null,
     };
   },
 
   methods: {
-
     // 新しいファイルがアップロードされたとき
     onUpload: function (event) {
       // 状態を初期化
@@ -138,7 +135,7 @@ export default {
 
       this.file = _file;
 
-      makeResizeIamgeFiles();
+      this.makeResizeIamgeFiles();
     },
 
     // 必要なサイズの画像を作成してダウンロードする
@@ -146,54 +143,54 @@ export default {
       this.resizeImgAndDownload(this.file, 160, 160);
     },
 
-
     // 指定されたサイズに画像をリサイズしてダウンロードする
-    resizeImgAndDownload:function(image_file,width,height) {
-      const canvas =  this.canvas;//document.createElement('canvas');
+    resizeImgAndDownload: function (image_file, width, height) {
+      const canvas = this.canvas; //document.createElement('canvas');
       canvas.width = width;
       canvas.height = height;
       const ctx = this.context;
-      const imageBitmapPromise =createImageBitmap(image_file)
+      const imageBitmapPromise = createImageBitmap(image_file);
 
-      imageBitmapPromise// 結果を受け取る
-		  .then(
+      imageBitmapPromise // 結果を受け取る
+        .then(
+          // ------------------------------------------------------------
+          // 成功時に実行されるコールバック関数
+          // ------------------------------------------------------------
+          function success(image_bitmap) {
+            // ------------------------------------------------------------
+            // ImageBitmap オブジェクトを描画する
+            // ------------------------------------------------------------
+            ctx.drawImage(image_bitmap, 0, 0);
 
-			// ------------------------------------------------------------
-			// 成功時に実行されるコールバック関数
-			// ------------------------------------------------------------
-			function success(image_bitmap){
+            // var canvas = document.getElementById(canvas_id);
+            //アンカータグを作成
+            var a = document.createElement('a');
+            //canvasをJPEG変換し、そのBase64文字列をhrefへセット
+            a.href = canvas.toDataURL('image/png');
+            //ダウンロード時のファイル名を指定
+            // 「元のファイル名+大きさ」
+            // 元のファイル名は拡張子はいらないので削除
+            var reg = /(.*)(?:\.([^.]+$))/;
+            a.download =
+              image_file.name.match(reg)[1] +
+              '_' +
+              width +
+              '_×' +
+              height +
+              '.jpg';
+            //クリックイベントを発生させる
+            a.click();
+          },
 
-				// ------------------------------------------------------------
-				// ImageBitmap オブジェクトを描画する
-				// ------------------------------------------------------------
-				ctx.drawImage( image_bitmap , 0 , 0);
-
-      // var canvas = document.getElementById(canvas_id);
-	    //アンカータグを作成
-     	var a = document.createElement('a');
-	    //canvasをJPEG変換し、そのBase64文字列をhrefへセット
-	    a.href = canvas.toDataURL('image/png');
-	    //ダウンロード時のファイル名を指定
-      // 「元のファイル名+大きさ」
-      // 元のファイル名は拡張子はいらないので削除
-      var reg=/(.*)(?:\.([^.]+$))/; 
-	    a.download = image_file.name.match(reg)[1]+'_'+width+'_×'+height+'.jpg';
-	    //クリックイベントを発生させる
-	    a.click();
-
-			},
-
-			// ------------------------------------------------------------
-			// 失敗時に実行されるコールバック関数
-			// ------------------------------------------------------------
-			function failure(e){
-				// 出力テスト
-				console.log(e);
-			}
-		);
-      },
-
-
+          // ------------------------------------------------------------
+          // 失敗時に実行されるコールバック関数
+          // ------------------------------------------------------------
+          function failure(e) {
+            // 出力テスト
+            console.log(e);
+          }
+        );
+    },
   },
 
   watch: {
@@ -209,9 +206,9 @@ export default {
     },
   },
 
-  mounted: function (){
-    this.canvas = this.$refs.canvas
-    this.context = this.canvas.getContext("2d")
+  mounted: function () {
+    this.canvas = this.$refs.canvas;
+    this.context = this.canvas.getContext('2d');
   },
 };
 </script>
